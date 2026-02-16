b0da3dc801d2ff35c800d86d8ac80635115f544f
ali-imtiyazkhan <itzkhantijara@gmail.com>
2026-02-16 22:56:15 +0530

    linked list code update

--- LinkedList/marge_to_list.cpp
+++ LinkedList/marge_to_list.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
 using namespace std;
 
-// Definition of ListNode
-struct ListNode {
+struct ListNode
+{
     int val;
-    ListNode* next;
-    
-    ListNode(int x) {
+    ListNode *next;
+
+    ListNode(int x)
+    {
         val = x;
         next = NULL;
     }
 };
 
-// Function to merge two sorted lists
-ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    // Create a dummy node
-    ListNode* dummy = new ListNode(0);
-    ListNode* tail = dummy;
 
-    while (list1 != NULL && list2 != NULL) {
-        if (list1->val <= list2->val) {
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+
+    ListNode *dummy = new ListNode(0);
+    ListNode *tail = dummy;
+
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->val <= list2->val)
+        {
             tail->next = list1;
             list1 = list1->next;
-        } else {
+        }
+        else
+        {
             tail->next = list2;
             list2 = list2->next;
         }
         tail = tail->next;
     }
 
-    // Attach remaining nodes
-    if (list1 != NULL) {
+
+    if (list1 != NULL)
+    {
         tail->next = list1;
-    } else {
+    }
+    else
+    {
         tail->next = list2;
     }
 
@@ -40,24 +49,27 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 }
 
 // Helper function to print list
-void printList(ListNode* head) {
-    while (head != NULL) {
+void printList(ListNode *head)
+{
+    while (head != NULL)
+    {
         cout << head->val << " ";
         head = head->next;
     }
 }
 
-int main() {
-  
-    ListNode* list1 = new ListNode(1);
+int main()
+{
+
+    ListNode *list1 = new ListNode(1);
     list1->next = new ListNode(2);
     list1->next->next = new ListNode(4);
 
-    ListNode* list2 = new ListNode(1);
+    ListNode *list2 = new ListNode(1);
     list2->next = new ListNode(3);
     list2->next->next = new ListNode(4);
 
-    ListNode* result = mergeTwoLists(list1, list2);
+    ListNode *result = mergeTwoLists(list1, list2);
 
     printList(result);
 
