32d20b3fcfe300b5cf9f4b3514b4197d011ac786
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-03-19 00:22:26 +0530

    solution : leetcode no 2

---
+++ LinkedList/Add_two.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n], b[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    int carry = 0;
+    int sum[n];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int s = a[i] + b[i] + carry;
+        sum[i] = s % 10;
+        carry = s / 10;
+    }
+
+    if (carry)
+        cout << carry << " ";
+    for (int i = 0; i < n; i++)
+        cout << sum[i] << " ";
+}
